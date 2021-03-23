42063301ce47099897b592a067526f4f2c08a9bf
Luzimar Pereira de Oliveira <luzimar.oliveira@ndd.com.br>
2021-03-22 21:45:36 -0300

    #2 Criado componentes dashboard e summary

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+import Summary from '../Summary';
+
+export default function Dashboard() {
+    return (
+        <Container>
+            <Summary />
+        </Container>
+    );
+}
