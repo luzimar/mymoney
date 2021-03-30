71717dcc9dbdedc15c791f3e7f833c213183d84e
Luzimar Pereira de Oliveira <luzimar.oliveira@ndd.com.br>
2021-03-30 19:47:48 -0400

    #3 Criado componente transactions table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from './styles';
 import Summary from '../Summary';
+import TransactionsTable from '../TransactionsTable';
 
 export default function Dashboard() {
     return (
         <Container>
             <Summary />
+            <TransactionsTable />
         </Container>
     );
 }
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from './styles';
+
+export default function TransactionsTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Consultoria</td>
+                        <td className="deposit">R$1.000,00</td>
+                        <td>Consultoria técnica</td>
+                        <td>20/03/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Consultoria</td>
+                        <td className="withdraw">- R$600,00</td>
+                        <td>Aluguel</td>
+                        <td>20/03/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    )
+}
